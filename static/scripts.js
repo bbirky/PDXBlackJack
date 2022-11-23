505006c0055ad0ea94963df9b19a132c9b836722
bbirky <bbirky907@gmail.com>
2022-11-22 21:33:04 -0800

    11-22

--- static/scripts.js
+++ static/scripts.js
@@ -2,22 +2,49 @@ const app = Vue.createApp({
     delimiters: ['[[', ']]'],
     data(){
         return{
+            cardImage: '',
+            cardImage2: '',
+            cardImage3: '',
+            cardImage4: ''
         }
     },
     methods:{
-        lastPage(){
+        createDeck(){
             axios({
                 method: 'get',
-                url: "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1", 
+                url: "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6", 
             }).then((response) => {
                 console.log(response)
+            }).catch((error) => {
+                console.log(error);
+            });
+        },
+        drawCard(){
+            axios({
+                method: 'get',
+                url: "https://www.deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2", 
+            }).then((response) => {
+                console.log(response)
+            }).catch((error) => {
+                console.log(error);
+            });
+        },
+        getCardImage(){
+            axios({
+                method: 'get',
+                url: "https://www.deckofcardsapi.com/api/deck/new/draw/?count=4", 
+            }).then((response) => {
+                this.cardImage = response.data.cards[0].image
+                this.cardImage2 = response.data.cards[1].image
+                this.cardImage3 = response.data.cards[2].image
+                this.cardImage4 = response.data.cards[3].image
             }).catch((error) => {
                 console.log(error);
             });
         }
     },
     mounted(){
-        this.lastPage(),
+        this.getCardImage(),
         console.log('mounted')
     },
 })
