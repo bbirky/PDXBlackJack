ac3c139eba9234ac5869cd1d84fda291a7314e19
bbirky <bbirky907@gmail.com>
2022-12-02 21:30:25 -0800

    12/02

--- static/scripts.js
+++ static/scripts.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
             playerCards:[],
             playerScore: 0,
             dealerScore: 0,
+            handInSession: false,
+            playerWinCount: 0,
+            playerLossCount: 0,
+            playerPushCount: 0,
+            announceWinner: "",
         }
     },
     methods:{
@@ -30,6 +35,7 @@ const app = Vue.createApp({
                     method: 'get',
                     url: `https://www.deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=4`,
                 }).then((response) => {
+                    this.announceWinner = ""
                     this.playerCards = []
                     this.dealerCards = []
                     this.playerCards.push(response.data.cards[0])
@@ -38,6 +44,7 @@ const app = Vue.createApp({
                     this.dealerCards.push(response.data.cards[3])
                     this.evaluatePlayerScore()
                     this.evaluateDealerScore()
+                    this.handInSession = true
                 }).catch((error) => {
                     console.log(error);
                 });
@@ -52,7 +59,10 @@ const app = Vue.createApp({
             }).then((response) => {
                 this.playerCards.push(response.data.cards[0])
                 this.evaluatePlayerScore()
-                
+                if (this.playerScore > 21 ) {
+                    this.handInSession = false;
+                    this.stand();
+                }
             }).catch((error) => {
                 console.log(error);
             });
@@ -66,11 +76,17 @@ const app = Vue.createApp({
                     this.dealerCards.push(response.data.cards[0])
                     this.evaluateDealerScore()
                     if (this.dealerScore < 17 ){
-                        this.stand()
+                        this.stand();
+                    } else {
+                        this.evaluateWinner();
+                        this.handInSession = false;
                     }
                 }).catch((error) => {
                     console.log(error);
                 });
+            } else {
+                this.evaluateWinner();
+                this.handInSession = false;
             }   
         },
         evaluatePlayerScore(){
@@ -105,6 +121,27 @@ const app = Vue.createApp({
                 }
             }
         },
+        evaluateWinner(){
+            this.announceWinner = ""
+            if (this.playerScore > this.dealerScore && this.playerScore <= 21){
+                this.playerWinCount += 1;
+                this.announceWinner = "You Win!";
+            } else if (this.playerScore < this.dealerScore && this.dealerScore <= 21){
+                this.playerLossCount += 1;
+                this.announceWinner = "You Lose...";
+            } else if (this.playerScore > 21 && this.dealerScore <= 21){
+                this.playerLossCount += 1;
+                this.announceWinner = "You Lose...";
+            } else if (this.playerScore <= 21 && this.dealerScore > 21){
+                this.playerWinCount += 1;
+                this.announceWinner = "You Win!";
+            } else if (this.playerScore == this.dealerScore && this.playerScore <= 21){
+                this.playerPushCount += 1;
+                this.announceWinner = "PUSH ...Not Great, Not Terrible...";
+            } else {
+                this.announceWinner = "";
+            }
+        }
     },
     mounted(){
         this.createDeck(),
